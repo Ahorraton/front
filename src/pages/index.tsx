10d696966dd54dc1340b2015ded52ebf58efb3fb
Merok23 <alejocalizblanco@gmail.com>
2024-05-23 12:09:53 -0300

    Add a basic button connected to the gateway + MUI to the package.json

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,54 @@
-import Head from "next/head";
-import Image from "next/image";
-import { Inter } from "next/font/google";
+import { useState } from "react";
+import { Box, Button } from "@mui/material";
 import styles from "@/styles/Home.module.css";
 
-const inter = Inter({ subsets: ["latin"] });
+interface Product {
+  id: number;
+  name: string;
+  price: number;
+  price_per_unit: number;
+}
+
+const URI = 'http://localhost:8000/products'; /**Move to a better place */
 
 export default function Home() {
+  const [error, setError] = useState<string | null>(null);
+  const [products, setProducts] = useState<Product[]>([]);
+
+  const handleClick = () => {
+    fetch(URI)
+      .then(response => response.json())
+      .then(data => {
+        const productsArray = data as Product[];
+        console.log(productsArray);
+        setProducts([...products, ...productsArray]);
+      })
+      .catch(error => {
+      setError(error.message);
+      });
+  }
+
   return (
     <>
-      <p>Hello World!</p>
-      <p>Thanks Next.js</p>
+      <Box className={styles.main}>
+        <p>Hello World!</p>
+        <Button
+          variant='contained'
+          onClick={handleClick}
+          color='primary'>
+          Click me!
+        </Button>
+        <Box>
+          {error && <p>{error}</p>}
+          <ul>
+            {products.map(product => (
+              <li key={product.id}>
+                {product.name} - ${product.price} - ${product.price_per_unit}
+              </li>
+            ))}
+          </ul>
+        </Box>
+      </Box>
     </>
   );
 }
