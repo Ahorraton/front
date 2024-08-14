dac41f9dde5e4f59f60b149be0c1ff61df1d71e2
ivanovic99 <ivanerlich@gmail.com>
2024-08-14 19:09:52 -0300

    Register, login and logout endpoints

---
+++ src/app/(ahorratonLayout)/components/user/logout/api/route.ts
@@ -0,0 +1,32 @@
+import { NextResponse } from 'next/server';
+import { serialize } from 'cookie';
+import axios from 'axios'
+
+export const dynamic = 'force-dynamic';
+
+const COOKIE_NAME = 'Ahorraton';
+const LOGOUT = '/logout'
+
+export async function GET() {
+    const serialized = serialize(COOKIE_NAME, "", {
+        httpOnly: true,
+        secure: process.env.NODE_ENV !== "development",
+        sameSite: "strict",
+        maxAge: -1,
+        path: "/",
+    });
+
+    try {
+        await axios.post(`${process.env.NEXT_PUBLIC_BACKEND_ENDPOINT}${LOGOUT}`);
+    } catch (error) {
+        console.error("Error logging out from backend:", error);
+    }
+
+    return new NextResponse(
+        JSON.stringify({ status: "success" }),
+        {
+            status: 200,
+            headers: { "Set-Cookie": serialized },
+        }
+    );
+}
