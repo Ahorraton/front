a312e9f0135dd5b4b2a1421f712a907cca505ad2
ivanovic99 <ivanerlich@gmail.com>
2024-08-28 17:24:07 -0300

    Update: userState has now userInfo --> username

--- src/redux/store/userSlice.ts
+++ src/redux/store/userSlice.ts
@@ -2,24 +2,29 @@ import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
 interface UserState {
     isLoggedIn: boolean;
+    userInfo: {
+        username: string;
+    } | null;
 }
 
 const initialState: UserState = {
     isLoggedIn: false,
+    userInfo: null,
 };
 
 const userSlice = createSlice({
     name: 'user',
     initialState,
     reducers: {
-        login: (state) => {
+        login: (state, action: PayloadAction<{ username: string }>) => {
             state.isLoggedIn = true;
+            state.userInfo = action.payload;
         },
         logout: (state) => {
             state.isLoggedIn = false;
+            state.userInfo = null;
         },
     },
 });
-
 export const { login, logout } = userSlice.actions;
 export default userSlice.reducer;
