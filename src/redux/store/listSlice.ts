edc34eaf2200783cda920016667f9878fe0d0482
ivanovic99 <ivanerlich@gmail.com>
2024-09-07 16:23:37 -0300

    Update: remove item to remove one quantity at a time

--- src/redux/store/listSlice.ts
+++ src/redux/store/listSlice.ts
@@ -28,6 +28,16 @@ const listSlice = createSlice({
             }
         },
         removeItem: (state, action: PayloadAction<string>) => {
+            const existingItem = state.items.find(item => item.id === action.payload);
+            if (existingItem) {
+                if (existingItem.quantity > 1) {
+                    existingItem.quantity -= 1;
+                } else {
+                    state.items = state.items.filter(item => item.id !== action.payload);
+                }
+            }
+        },
+        deleteItem: (state, action: PayloadAction<string>) => {
             state.items = state.items.filter(item => item.id !== action.payload);
         },
         clearList: (state) => {
@@ -36,5 +46,5 @@ const listSlice = createSlice({
     },
 });
 
-export const { addItem, removeItem, clearList } = listSlice.actions;
+export const { addItem, removeItem, deleteItem, clearList } = listSlice.actions;
 export default listSlice.reducer;
