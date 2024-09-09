daa1ad8996b6fa36d8daeea186a24c50c1249c2e
ivanovic99 <ivanerlich@gmail.com>
2024-09-09 16:10:21 -0300

    Update: Using ean instead of Id

--- src/app/(ahorratonLayout)/components/list/ListItemComponent.tsx
+++ src/app/(ahorratonLayout)/components/list/ListItemComponent.tsx
@@ -3,29 +3,31 @@ import { ListItem, ListItemText, ListItemSecondaryAction, IconButton } from '@mu
 import AddCircleIcon from '@mui/icons-material/AddCircle';
 import RemoveCircleIcon from '@mui/icons-material/RemoveCircle';
 import DeleteIcon from '@mui/icons-material/Delete';
+import { ListItem as ListItemType } from '../../../../redux/store/listSlice';
+
 
 interface ListItemComponentProps {
-    item: { id: string; name: string; quantity: number; price: number };
-    onAdd: (id: string) => void;
-    onRemove: (id: string) => void;
-    onDelete: (id: string) => void;
+    item: ListItemType;
+    onAdd: (ean: string) => void;
+    onRemove: (ean: string) => void;
+    onDelete: (ean: string) => void;
 }
 
 const ListItemComponent: React.FC<ListItemComponentProps> = ({ item, onAdd, onRemove, onDelete }) => {
     return (
-        <ListItem key={item.id}>
+        <ListItem key={item.ean}>
             <ListItemText
                 primary={item.name}
                 secondary={`Cantidad: ${item.quantity}`}
             />
             <ListItemSecondaryAction>
-                <IconButton edge="end" aria-label="add" onClick={() => onAdd(item.id)}>
+                <IconButton edge="end" aria-label="add" onClick={() => onAdd(item.ean)}>
                     <AddCircleIcon />
                 </IconButton>
-                <IconButton edge="end" aria-label="remove" onClick={() => onRemove(item.id)}>
+                <IconButton edge="end" aria-label="remove" onClick={() => onRemove(item.ean)}>
                     <RemoveCircleIcon />
                 </IconButton>
-                <IconButton edge="end" aria-label="delete" onClick={() => onDelete(item.id)}>
+                <IconButton edge="end" aria-label="delete" onClick={() => onDelete(item.ean)}>
                     <DeleteIcon />
                 </IconButton>
             </ListItemSecondaryAction>
