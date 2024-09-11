f18b13307da1a4e48e77e4dd99522d031d600495
ivanovic99 <ivanerlich@gmail.com>
2024-09-11 18:19:26 -0300

    Add: multiple lists slice added to redux

--- src/redux/store/index.ts
+++ src/redux/store/index.ts
@@ -3,6 +3,7 @@ import { persistStore, persistReducer } from 'redux-persist';
 import cookieStorage from './cookieStorage';
 import userReducer from './userSlice';
 import listReducer from './listSlice';
+import multipleListsReducer from './multipleListsSlice';
 
 const persistConfig = {
   key: 'root',
@@ -12,6 +13,7 @@ const persistConfig = {
 const rootReducer = {
   user: persistReducer(persistConfig, userReducer),
   list: persistReducer(persistConfig, listReducer),
+  multipleLists: persistReducer(persistConfig, multipleListsReducer),
 };
 
 export const store = configureStore({
