f7a1a17418d9314bef7a45d4d0720e018e4ec143
Luis <luisjosepr@gmail.com>
2024-10-26 12:42:42 -0300

    feat: init state of hero component with slider

--- src/app/(ahorratonLayout)/layout/NavBarDesktop.tsx
+++ src/app/(ahorratonLayout)/layout/NavBarDesktop.tsx
@@ -1,49 +1,53 @@
-import React from 'react';
-import { Box, ButtonBase, Typography } from '@mui/material';
-import SearchBar from './SearchBar';
-import UserIcon from '../components/user/UserIcon';
-import ListIcon from '../components/list/ListIcon';
-import './nav_bar.css';
+import React from "react";
+import { Box, ButtonBase, Typography } from "@mui/material";
+import SearchBar from "./SearchBar";
+import UserIcon from "../components/user/UserIcon";
+import ListIcon from "../components/list/ListIcon";
+import "./nav_bar.css";
 
 interface NavBarDesktopProps {
-    query: string;
-    setQuery: React.Dispatch<React.SetStateAction<string>>;
-    compareSearch: boolean;
-    setCompareSearch: (e: boolean) => void;
+  query: string;
+  setQuery: React.Dispatch<React.SetStateAction<string>>;
+  compareSearch: boolean;
+  setCompareSearch: (e: boolean) => void;
 }
 
 const NavBarDesktop: React.FC<NavBarDesktopProps> = ({
-    query,
-    setQuery,
-    compareSearch,
-    setCompareSearch,
+  query,
+  setQuery,
+  compareSearch,
+  setCompareSearch,
 }) => {
-    return (
-        <Box className="nav-bar-layout">
-            <Box className='title-layout' width='20vw'>
-                <ButtonBase disableRipple href="/" className="title-hover">
-                    <Typography variant="h1" className='spin-on-hover'>
-                        🐭
-                    </Typography>
-                    <Typography variant="h1">
-                        Ahorratón
-                    </Typography>
-                </ButtonBase>
-            </Box>
-            <Box width='70vw'>
-                <SearchBar
-                    starting_query={query}
-                    set={setQuery}
-                    compareSearch={compareSearch}
-                    setCompareSearch={setCompareSearch}
-                />
-            </Box>
-            <Box className='about-us-layout' width='10vw' display="flex" justifyContent="flex-end">
-                <ListIcon />
-                <UserIcon />
-            </Box>
-        </Box>
-    );
+  return (
+    <Box className="nav-bar-layout">
+      <Box className="title-layout" width="20vw">
+        <ButtonBase disableRipple href="/" className="title-hover">
+          <Typography variant="h1" className="spin-on-hover">
+            🐭
+          </Typography>
+          <Typography variant="h1">Ahorratón</Typography>
+        </ButtonBase>
+      </Box>
+      <Box width="70vw">
+        <SearchBar
+          starting_query={query}
+          set={setQuery}
+          compareSearch={compareSearch}
+          setCompareSearch={setCompareSearch}
+        />
+      </Box>
+      <Box
+        className="about-us-layout"
+        width="10vw"
+        display="flex"
+        justifyContent="flex-center"
+        alignItems="center"
+      >
+        <ListIcon />
+        <UserIcon />
+      </Box>
+    </Box>
+  );
 };
 
 export default NavBarDesktop;
--- src/app/(ahorratonLayout)/page.tsx
+++ src/app/(ahorratonLayout)/page.tsx
@@ -1,32 +1,50 @@
-"use client"
+"use client";
 import React, { useEffect } from "react";
 import { Box, Skeleton, Typography } from "@mui/material";
 import { Button } from "@mui/material";
-import { Grid } from '@mui/material';
-import PageContainer from '@/app/(ahorratonLayout)/components/container/PageContainer';
-import Product from '@/app/(ahorratonLayout)/components/types/Product';
-import { fetch_async } from '@/app/(ahorratonLayout)/async/common/fetch_async';
-import ProductGrid from '@/app/(ahorratonLayout)/components/product_search/ProductGrid';
-import './landing_page.css'
-
-
+import { Grid } from "@mui/material";
+import PageContainer from "@/app/(ahorratonLayout)/components/container/PageContainer";
+import Product from "@/app/(ahorratonLayout)/components/types/Product";
+import { fetch_async } from "@/app/(ahorratonLayout)/async/common/fetch_async";
+import ProductGrid from "@/app/(ahorratonLayout)/components/product_search/ProductGrid";
+import "./landing_page.css";
+import HeroSection from "./components/heroSection";
 
 export default function Home() {
   const [loading, setLoading] = React.useState<boolean>(true);
   const [products, setProducts] = React.useState<Product[]>([]);
   const [error, setError] = React.useState<string | null>(null);
+  const [recipes, setRecipes] = React.useState<any[]>([]);
 
   const skeletonGridItems = Array.from({ length: 8 }, (_, i) => (
     <Grid item key={i} xs={12} sm={6} md={4} lg={3}>
-      <Skeleton variant="rounded" animation='wave' height={350} width={280} />
+      <Skeleton variant="rounded" animation="wave" height={350} width={280} />
     </Grid>
   ));
 
   useEffect(() => {
+    const fetchRecipes = async () => {
+      try {
+        const res = await fetch_async("/recipes");
+        console.log("Recetas recibidas");
+        console.log(res);
+        const recipes_result: any[] = res.recipes ? res.recipes : [];
+        setRecipes(recipes_result);
+      } catch (e: unknown) {
+        setError("error");
+        throw new Error(String(e));
+      }
+    };
+    fetchRecipes();
+  }, []);
+
+  useEffect(() => {
     const fetchProducts = async () => {
       try {
-        const res = await fetch_async('/products/' + "gaseosa" + '?offset=0' + '&limit=8');
-        const products_result : Product[] = res.products ? res.products : [];
+        const res = await fetch_async(
+          "/products/" + "gaseosa" + "?offset=0" + "&limit=8"
+        );
+        const products_result: Product[] = res.products ? res.products : [];
         setProducts(products_result);
         setLoading(false);
       } catch (e: unknown) {
@@ -36,26 +54,25 @@ export default function Home() {
     };
 
     fetchProducts();
-  }
-  , []);
+  }, []);
 
   return (
     <PageContainer title="Ahorraton" description="Ahorra en grande">
-      <Box className='page-layout'>
-      {
-        loading ? (
+      {/* I want a section with a slider that contains a big image that cover 70% of the screen and the description taking the rest of the space left*/}
+      <HeroSection recipes={recipes} />
+      <Box className="page-layout">
+        {loading ? (
           <Grid container spacing={2}>
             {skeletonGridItems}
           </Grid>
+        ) : products.length === 0 ? (
+          <Typography variant="h6" align="center">
+            No se encontraron productos.
+          </Typography>
         ) : (
-          products.length === 0 ? (
-            <Typography variant="h6" align="center">No se encontraron productos.</Typography>
-          ) : (
-            <ProductGrid products={products}/>
-          )
-        )
-      }
+          <ProductGrid products={products} />
+        )}
       </Box>
     </PageContainer>
   );
-};
+}
