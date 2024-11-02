c5d1e3fd80cab3a51dc9e6b3df0cc0c54e5a1cd2
Luis <luisjosepr@gmail.com>
2024-11-02 15:22:00 -0300

    feat: updated featured products home page

--- src/app/(ahorratonLayout)/components/product_search/ProductGrid.tsx
+++ src/app/(ahorratonLayout)/components/product_search/ProductGrid.tsx
@@ -1,17 +1,43 @@
 import React from "react";
-import { Grid } from "@mui/material";
+import { Box, Grid, Typography } from "@mui/material";
 import Product from "../types/Product";
 import ProductCard from "./ProductCard";
+import { addItem } from "@/redux/store/listSlice";
+import { useDispatch } from "react-redux";
 
 const FeaturedProducts = ({ products }: { products: Product[] }) => {
+  const dispatch = useDispatch();
+
+  const handleAddProduct = (product: Product) => {
+    const productToSave = {
+      ean: String(product.id),
+      name: product.name,
+      quantity: 1,
+    };
+    dispatch(addItem(productToSave));
+  };
+
+  const handleOnClickSearch = (prod_name: string) => {
+    console.log("Buscando producto: ", prod_name);
+  };
+
   return (
-    <Grid container spacing={2}>
-      {products.map((product) => (
-        <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
-          <ProductCard product={product} />
-        </Grid>
-      ))}
-    </Grid>
+    <Box py={4} p={4}>
+      <Typography variant="h4" gutterBottom>
+        Featured Products
+      </Typography>
+      <Grid container spacing={2}>
+        {products.map((product) => (
+          <Grid item key={product.id} xs={12} sm={6} md={4} lg={4}>
+            <ProductCard
+              product={product}
+              addProduct={handleAddProduct}
+              onClickSearch={handleOnClickSearch}
+            />
+          </Grid>
+        ))}
+      </Grid>
+    </Box>
   );
 };
 
