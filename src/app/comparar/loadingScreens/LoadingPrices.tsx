85abbdce279d5e839d9209d47a4c9e519650e461
Luis <luisjosepr@gmail.com>
2024-11-07 19:57:56 -0300

    refactor: moved files to endpoint

---
+++ src/app/comparar/loadingScreens/LoadingPrices.tsx
@@ -0,0 +1,31 @@
+import { Box, CircularProgress } from "@mui/material";
+import "./loadingPrices.css"; // Import the CSS file
+
+export const LoadingCompareScreen = () => {
+  return (
+    <Box>
+      <Box
+        aria-label="Orange and tan hamster running in a metal wheel"
+        role="img"
+        className="wheel-and-hamster"
+      >
+        <Box className="wheel"></Box>
+        <Box className="hamster">
+          <Box className="hamster__body">
+            <Box className="hamster__head">
+              <Box className="hamster__ear"></Box>
+              <Box className="hamster__eye"></Box>
+              <Box className="hamster__nose"></Box>
+            </Box>
+            <Box className="hamster__limb hamster__limb--fr"></Box>
+            <Box className="hamster__limb hamster__limb--fl"></Box>
+            <Box className="hamster__limb hamster__limb--br"></Box>
+            <Box className="hamster__limb hamster__limb--bl"></Box>
+            <Box className="hamster__tail"></Box>
+          </Box>
+        </Box>
+        <Box className="spoke"></Box>
+      </Box>
+    </Box>
+  );
+};
