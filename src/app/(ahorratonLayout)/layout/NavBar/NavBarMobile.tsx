1a4f54326e3c1153e3549dccca6b8c22fdfb9681
Luis <luisjosepr@gmail.com>
2024-11-08 12:16:06 -0300

    refactor: moved navbar components

---
+++ src/app/(ahorratonLayout)/layout/NavBar/NavBarMobile.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { Box, ButtonBase, Typography } from "@mui/material";
+import SearchBar from "./SearchBar";
+import UserIcon from "../../components/user/UserIcon";
+import ListIcon from "../../components/list/ListIcon";
+import "./nav_bar.css";
+import NavBarProps from "./NavBarProps";
+
+const NavBarMobile: React.FC<NavBarProps> = ({ query, setQuery }) => {
+  return (
+    <Box className="nav-bar-mobile">
+      <Box
+        className="title-layout"
+        width="100%"
+        padding="10px"
+        display="flex"
+        alignItems="center"
+      >
+        <ListIcon />
+        <ButtonBase
+          disableRipple
+          href="/"
+          className="title-button"
+          sx={{ flexGrow: 1, textAlign: "center" }}
+        >
+          <Typography
+            variant="h6"
+            className="title-text"
+            sx={{ fontSize: "1.5rem" }}
+          >
+            Ahorratón 🐭
+          </Typography>
+        </ButtonBase>
+
+        <UserIcon />
+      </Box>
+      <Box width="100%" padding="10px">
+        <SearchBar starting_query={query} set={setQuery} />
+      </Box>
+    </Box>
+  );
+};
+
+export default NavBarMobile;
