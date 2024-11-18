2b11ec4a5bb6a42719acc95524b06cb101dc91f0
Luis <luisjosepr@gmail.com>
2024-11-18 19:29:19 -0300

    fix

--- src/app/(ahorratonLayout)/components/product_search/GridFilter.tsx
+++ src/app/(ahorratonLayout)/components/product_search/GridFilter.tsx
@@ -1,131 +1,143 @@
-import React, { useState } from 'react';
-import TextField from '@mui/material/TextField';
-import { Box, Button, Typography } from '@mui/material';
-import { Checkbox, FormControlLabel } from '@mui/material';
-import { Accordion, AccordionSummary, AccordionDetails } from '@mui/material';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import Filters from "@/app/buscar/types/Filters";
-import './filter_styles.css';
+import React, { useState } from "react";
+import TextField from "@mui/material/TextField";
+import { Box, Button, Typography } from "@mui/material";
+import { Checkbox, FormControlLabel } from "@mui/material";
+import { Accordion, AccordionSummary, AccordionDetails } from "@mui/material";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 
-const ProductFilters = ({fetchFunc}: { fetchFunc: (filters: Filters | null) => void}) => {
-    const markets = ['coto', 'disco', 'vea', 'carrefour', 'jumbo', 'dia'];
-    const [minPrice, setMinPrice] = useState<number>(0);
-    const [maxPrice, setMaxPrice] = useState<number>(Number.MAX_SAFE_INTEGER);
-    const [selectedMarkets, setSelectedMarkets] = useState<string[]>(markets);
+import "./filter_styles.css";
+import Filters from "@/app/types/Filters";
 
-    const handleMarketChange = (market: string) => {
-        if (selectedMarkets.includes(market)) {
-            setSelectedMarkets(selectedMarkets.filter((m) => m !== market));
-        } else {
-            setSelectedMarkets([...selectedMarkets, market]);
-        }
-    };
+const ProductFilters = ({
+  fetchFunc,
+}: {
+  fetchFunc: (filters: Filters | null) => void;
+}) => {
+  const markets = ["coto", "disco", "vea", "carrefour", "jumbo", "dia"];
+  const [minPrice, setMinPrice] = useState<number>(0);
+  const [maxPrice, setMaxPrice] = useState<number>(Number.MAX_SAFE_INTEGER);
+  const [selectedMarkets, setSelectedMarkets] = useState<string[]>(markets);
 
-    const handleTextChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>, max: boolean = true) => {
-        const value = e.target.value;
-        if (!/^\d*$/.test(value)) {
-            return;
-        }
-        if (value === '') {
-            if (max) {
-                setMaxPrice(Number.MAX_SAFE_INTEGER);
-            }
-            else {
-                setMinPrice(0);
-            }
-        } else {
-            const price = Number(value);
-            if (price >= 0) {
-                if (max) {
-                    setMaxPrice(price);
-                } else {
-                    setMinPrice(price);
-                }
-            }
-        }
+  const handleMarketChange = (market: string) => {
+    if (selectedMarkets.includes(market)) {
+      setSelectedMarkets(selectedMarkets.filter((m) => m !== market));
+    } else {
+      setSelectedMarkets([...selectedMarkets, market]);
     }
+  };
 
-    const handleApplyClick = () => {
-        fetchFunc({
-            markets: selectedMarkets,
-            min_price: minPrice,
-            max_price: maxPrice,
-        });
+  const handleTextChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+    max: boolean = true
+  ) => {
+    const value = e.target.value;
+    if (!/^\d*$/.test(value)) {
+      return;
     }
+    if (value === "") {
+      if (max) {
+        setMaxPrice(Number.MAX_SAFE_INTEGER);
+      } else {
+        setMinPrice(0);
+      }
+    } else {
+      const price = Number(value);
+      if (price >= 0) {
+        if (max) {
+          setMaxPrice(price);
+        } else {
+          setMinPrice(price);
+        }
+      }
+    }
+  };
 
-    return (
-        <Accordion>
-            <AccordionSummary
-                sx={{
-                    '& .MuiAccordionSummary-content': {
-                        justifyContent: 'center',
-                    }
-                }}
-                expandIcon={<ExpandMoreIcon />}
-            >
-                <h3>Filtros</h3>
-            </AccordionSummary>
-            <AccordionDetails>
-                <Box sx={{
-                    display: 'flex',
-                    justifyContent: 'center',
-                }}>
-                    <Typography variant='h6'>Supermercados</Typography>
-                </Box>
-                <Box className='accordion-row'>
-                    {markets.map((market) => (
-                            <FormControlLabel
-                                key={market}
-                                control={
-                                    <Checkbox
-                                        checked={selectedMarkets.includes(market)}
-                                        onChange={() => handleMarketChange(market)}
-                                    />
-                                }
-                                label={market.charAt(0).toUpperCase() + market.slice(1)}
-                            />
-                    ))}
-                </Box>
-                <br />
-                <Box sx={{
-                    display: 'flex',
-                    justifyContent: 'center',
-                }}>
-                    <Typography variant='h6'>Precio</Typography>
-                </Box>
-                <br />
-                <Box className='accordion-row'>
-                    <TextField
-                        label="Mínimo"
-                        type="tel"
-                        value={minPrice === 0 ? '' : minPrice}
-                        onChange={(e) => handleTextChange(e, false)}
-                        sx = {{
-                            marginBottom: '5%',
-                        }}
-                    />
-                    <TextField
-                        label="Máximo"
-                        type="tel"
-                        value={maxPrice === Number.MAX_SAFE_INTEGER ? '' : maxPrice}
-                        onChange={(e) => handleTextChange(e, true)}
-                        sx = {{
-                            marginBottom: '5%',
-                        }}
-                    />
-                </Box>
-                <br/>
-                <Box className='accordion-row'>
-                    <Button
-                        variant='contained'
-                        color='secondary'
-                        onClick={handleApplyClick}>
-                        Aplicar
-                    </Button>
-                </Box>
-            </AccordionDetails>
-        </Accordion>
-    );
+  const handleApplyClick = () => {
+    fetchFunc({
+      markets: selectedMarkets,
+      min_price: minPrice,
+      max_price: maxPrice,
+    });
+  };
+
+  return (
+    <Accordion>
+      <AccordionSummary
+        sx={{
+          "& .MuiAccordionSummary-content": {
+            justifyContent: "center",
+          },
+        }}
+        expandIcon={<ExpandMoreIcon />}
+      >
+        <h3>Filtros</h3>
+      </AccordionSummary>
+      <AccordionDetails>
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+          }}
+        >
+          <Typography variant="h6">Supermercados</Typography>
+        </Box>
+        <Box className="accordion-row">
+          {markets.map((market) => (
+            <FormControlLabel
+              key={market}
+              control={
+                <Checkbox
+                  checked={selectedMarkets.includes(market)}
+                  onChange={() => handleMarketChange(market)}
+                />
+              }
+              label={market.charAt(0).toUpperCase() + market.slice(1)}
+            />
+          ))}
+        </Box>
+        <br />
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+          }}
+        >
+          <Typography variant="h6">Precio</Typography>
+        </Box>
+        <br />
+        <Box className="accordion-row">
+          <TextField
+            label="Mínimo"
+            type="tel"
+            value={minPrice === 0 ? "" : minPrice}
+            onChange={(e) => handleTextChange(e, false)}
+            sx={{
+              marginBottom: "5%",
+            }}
+          />
+          <TextField
+            label="Máximo"
+            type="tel"
+            value={maxPrice === Number.MAX_SAFE_INTEGER ? "" : maxPrice}
+            onChange={(e) => handleTextChange(e, true)}
+            sx={{
+              marginBottom: "5%",
+            }}
+          />
+        </Box>
+        <br />
+        <Box className="accordion-row">
+          <Button
+            variant="contained"
+            color="secondary"
+            onClick={handleApplyClick}
+          >
+            Aplicar
+          </Button>
+        </Box>
+      </AccordionDetails>
+    </Accordion>
+  );
 };
 
 export default ProductFilters;
