0a963879124e78236aec11efb9071ffaaf110e24
Luis <luisjosepr@gmail.com>
2024-11-18 23:52:26 -0300

    feat: added more recipes link

--- src/app/(ahorratonLayout)/components/featuredRecipes/GoToAllRecipesItem.tsx
+++ src/app/(ahorratonLayout)/components/featuredRecipes/GoToAllRecipesItem.tsx
@@ -1,11 +1,4 @@
-import {
-  Box,
-  Card,
-  CardActionArea,
-  CardContent,
-  CardMedia,
-  Icon,
-} from "@mui/material";
+import { Card, CardActionArea, CardContent, Link } from "@mui/material";
 import React from "react";
 import MenuBookIcon from "@mui/icons-material/MenuBook";
 import "./recipe.css";
@@ -13,22 +6,27 @@ import "./recipe.css";
 export default function GoToAllRecipesItem() {
   return (
     <Card id="see-more-recipes-card">
-      <CardActionArea
-        onClick={() => console.log("Hello")}
-        id="see-more-recipes-action-card"
-        sx={{
-          flexGrow: 1,
-          borderBottomLeftRadius: "0px",
-          borderBottomRightRadius: "0px",
-        }}
+      <Link
+        href="/recipes"
+        underline="none"
+        sx={{ color: "inherit", display: "block" }}
       >
-        <CardContent
-          id="see-more-recipes-card-content"
-          className="see-more-recipes"
+        <CardActionArea
+          id="see-more-recipes-action-card"
+          sx={{
+            flexGrow: 1,
+            borderBottomLeftRadius: "0px",
+            borderBottomRightRadius: "0px",
+          }}
         >
-          <MenuBookIcon />
-        </CardContent>
-      </CardActionArea>
+          <CardContent
+            id="see-more-recipes-card-content"
+            className="see-more-recipes"
+          >
+            <MenuBookIcon />
+          </CardContent>
+        </CardActionArea>
+      </Link>
     </Card>
   );
 }
