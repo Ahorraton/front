ee438f00a4fcda92cd4b5cb9a2854f09c7185f96
Luis <luisjosepr@gmail.com>
2024-11-19 00:00:15 -0300

    refactor

--- src/app/recipes/Recipe.tsx
+++ src/app/recipes/Recipe.tsx
@@ -17,50 +17,13 @@ import { Recipe } from "@/app/types/Recipe";
 import { fetch_async } from "../../utils/common/fetch_async";
 import "./recipe.css";
 
-export default function RecipePage() {
-  const [recipes, setRecipes] = useState<Recipe[]>([]);
+export default function RecipePage({ recipes }: { recipes: Recipe[] }) {
   const [selectedRecipe, setSelectedRecipe] = useState<number | null>(null);
-  const [loading, setLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
-
-  useEffect(() => {
-    const fetchRecipes = async () => {
-      try {
-        const res = await fetch_async("/recipes");
-        setLoading(false);
-        console.log("Recetas recibidas");
-        console.log(res);
-        const recipes_result: Recipe[] = res.recipes ? res.recipes : [];
-        setRecipes(recipes_result);
-      } catch (e: unknown) {
-        setError("error");
-        throw new Error(String(e));
-      }
-    };
-    fetchRecipes();
-  }, []);
 
   const handleRecipeClick = (recipeId: number) => {
     setSelectedRecipe(recipeId);
   };
-
-  if (loading) {
-    return (
-      <Box component="div">
-        <CircularProgress />;
-      </Box>
-    );
-  }
-
-  if (error) {
-    return (
-      <Box component="div" className=".error-message" id="recipes-error-box">
-        <Typography color="error">
-          Se produjo el siguiente error: {error}
-        </Typography>
-      </Box>
-    );
-  }
 
   return (
     <Box component="div" id="recipes-div">
--- src/app/recipes/page.tsx
+++ src/app/recipes/page.tsx
@@ -1,12 +1,51 @@
-import { Box } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import RecipePage from "./Recipe";
 import "./recipe.css";
+// import HeroSection from "../(ahorratonLayout)/components/heroSection";
+import { useEffect, useState } from "react";
+import { LoadingHamsterScreen } from "@/app/loadingScreens/loadingHamster/LoadingHamster";
+import { fetch_async } from "@/utils/common/fetch_async";
+import { Recipe } from "../types/Recipe";
 
 export default function Page() {
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+  const [recipes, setRecipes] = useState<Recipe[]>([]);
+
+  useEffect(() => {
+    const fetchRecipes = async () => {
+      try {
+        const res = await fetch_async("/recipes");
+        setLoading(false);
+        console.log("Recetas recibidas");
+        console.log(res);
+        const recipes_result: Recipe[] = res.recipes ? res.recipes : [];
+        setRecipes(recipes_result);
+      } catch (e: unknown) {
+        setError("error");
+        throw new Error(String(e));
+      }
+    };
+    fetchRecipes();
+  }, []);
+
   return (
-    <Box component="div" className="recipe-layout" id="recipe-style">
-      {/* <HeroSection recipes={recipes} /> */}
-      <RecipePage />
+    <Box>
+      {loading ? (
+        <Box component="div">
+          <LoadingHamsterScreen />;
+        </Box>
+      ) : error ? (
+        <Box component="div">
+          <Typography>Error {error}</Typography>
+        </Box>
+      ) : (
+        <Box component="div" className="recipe-layout" id="recipe-style">
+          {/* <HeroSection recipes={recipes} /> */}
+          <RecipePage recipes={recipes} />
+        </Box>
+      )}
+      ;
     </Box>
   );
 }
