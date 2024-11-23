511f1e33bc390cd76c4c5bb31ea524e6c9291c56
Luis <luisjosepr@gmail.com>
2024-11-23 09:33:08 -0300

    refactor: small improvements

--- src/app/recipes/Recipe.tsx
+++ src/app/recipes/Recipe.tsx
@@ -19,11 +19,7 @@ export default function RecipePage({ recipes }: { recipes: Recipe[] }) {
   };
 
   return (
-    <Box
-      component="div"
-      id="recipes-div"
-      className="recipes-layout-title-cards"
-    >
+    <Box>
       <Typography
         variant="h5"
         component="h2"
@@ -34,36 +30,42 @@ export default function RecipePage({ recipes }: { recipes: Recipe[] }) {
         Featured Recipes
       </Typography>
 
-      <Box component="div" id="recipes-grid-div" className="recipes-grid-div">
-        <Grid container spacing={4} id="recipes-grid">
-          {recipes.map((recipe) => (
-            <Grid item key={recipe.id} xs={12} sm={6} md={4}>
-              <Card
-                onClick={() => handleRecipeClick(recipe.id)}
-                sx={{ cursor: "pointer", textDecoration: "none" }}
-              >
-                <CardMedia
-                  component="img"
-                  height="200"
-                  image={recipe.img_url}
-                  alt={recipe.title}
-                />
-                <CardContent>
-                  <Typography gutterBottom variant="h6" component="div">
-                    {recipe.title}
-                  </Typography>
-                </CardContent>
-              </Card>
-            </Grid>
-          ))}
-        </Grid>
-        {selectedRecipe && (
-          <RecipeDetails
-            recipeId={selectedRecipe}
-            onClose={() => setSelectedRecipe(null)}
-            onAddList={() => console.log("Adding to list")}
-          />
-        )}
+      <Box
+        component="div"
+        id="recipes-div"
+        className="recipes-layout-title-cards"
+      >
+        <Box component="div" id="recipes-grid-div" className="recipes-grid-div">
+          <Grid container spacing={4} id="recipes-grid">
+            {recipes.map((recipe) => (
+              <Grid item key={recipe.id} xs={12} sm={6} md={4}>
+                <Card
+                  onClick={() => handleRecipeClick(recipe.id)}
+                  sx={{ cursor: "pointer", textDecoration: "none" }}
+                >
+                  <CardMedia
+                    component="img"
+                    height="200"
+                    image={recipe.img_url}
+                    alt={recipe.title}
+                  />
+                  <CardContent>
+                    <Typography gutterBottom variant="h6" component="div">
+                      {recipe.title}
+                    </Typography>
+                  </CardContent>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
+          {selectedRecipe && (
+            <RecipeDetails
+              recipeId={selectedRecipe}
+              onClose={() => setSelectedRecipe(null)}
+              onAddList={() => console.log("Adding to list")}
+            />
+          )}
+        </Box>
       </Box>
     </Box>
   );
