f01b910a261f9577d1529007de557fd6ff907aa7
Luis <luisjosepr@gmail.com>
2024-11-23 14:02:49 -0300

    refactor: added prod missing fields

--- src/app/types/Product.tsx
+++ src/app/types/Product.tsx
@@ -1,12 +1,14 @@
 export type Product = {
+  ean: string;
   id: number;
   name: string;
   price: number;
-  price_per_unit: number | null;
-  created_at: string;
-  market: string;
-  image_url: string | null;
-  ean: string;
-  url: string | null;
   quantity?: number;
+  unit?: string; // kg, Lts, g
+  market: string;
+  dir_sucursal?: string;
+  is_online?: string;
+  image_url: string | null;
+  url: string | null;
+  created_at: string;
 };
