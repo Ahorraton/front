0ea73551484e03d843de4c3af998739c26f4e199
Luis <luisjosepr@gmail.com>
2024-11-25 15:10:25 -0300

    fix

--- src/app/(ahorratonLayout)/components/featuredRecipes/GoToAllRecipesItem.tsx
+++ src/app/(ahorratonLayout)/components/featuredRecipes/GoToAllRecipesItem.tsx
@@ -5,7 +5,7 @@ import "./recipe.css";
 
 export default function GoToAllRecipesItem() {
   return (
-    <Card id="see-more-recipes-card" className="featured-recipe-card">
+    <Card id="see-more-recipes-card">
       <Link
         href="/recipes"
         underline="none"
@@ -13,6 +13,7 @@ export default function GoToAllRecipesItem() {
       >
         <CardActionArea
           id="see-more-recipes-action-card"
+          className="featured-recipe-card"
           sx={{
             flexGrow: 1,
             borderBottomLeftRadius: "0px",
