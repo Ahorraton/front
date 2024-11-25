2681886d6a9b1a72b97f9f6426a0cb1ac1494a8b
Luis <luisjosepr@gmail.com>
2024-11-25 13:09:59 -0300

    wip

--- src/app/recipes/RecipeDetails.tsx
+++ src/app/recipes/RecipeDetails.tsx
@@ -13,19 +13,19 @@ import {
 } from "@mui/material";
 
 import { Recipe } from "../types/Recipe";
+import "./recipe.css";
 
 interface RecipeDetailsProps {
   recipe: Recipe;
-  loading: boolean;
-  error: string | null;
+
   onClose: () => void;
   onAddList: () => void;
 }
 
+const DEFAULT_PROD_IMG = "/images/stock_product/rat.png";
+
 const RecipeDetails: React.FC<RecipeDetailsProps> = ({
   recipe,
-  loading,
-  error,
   onClose,
   onAddList,
 }) => {
@@ -35,51 +35,51 @@ const RecipeDetails: React.FC<RecipeDetailsProps> = ({
       onClose={onClose}
       aria-labelledby="recipe-dialog-title"
       id="recipe-details-dialog"
+      className="selected-recipe-layout"
     >
-      <DialogContent>
+      <DialogContent id="dialog-content-div" className="selected-recipe-layout">
+        <Box
+          component="img"
+          src={recipe.img_url}
+          onError={(e) => {
+            e.currentTarget.src = DEFAULT_PROD_IMG;
+          }}
+          id="selected-recipe-img"
+          className="selected-recipe-img"
+        />
         <Grid
           container
           spacing={2}
           className="selected-recipe-view-grid"
           id="selected-recipe-grid"
         >
-          <DialogTitle id="recipe-dialog-title">
-            {loading ? "Loading Recipe..." : recipe?.title}
-          </DialogTitle>
+          <DialogTitle id="recipe-dialog-title">{recipe?.title}</DialogTitle>
           <Box id="ingredients-and-add-button">
-            {loading ? (
-              <CircularProgress />
-            ) : error ? (
-              <Typography color="error">{`Error Loading Recipe Details: ${error}`}</Typography>
-            ) : (
-              <Box>
-                <Typography variant="h6" gutterBottom>
-                  Ingredients:
-                </Typography>
-                <ul>
-                  {recipe.items?.map((item, index) => (
-                    <li key={index}>{item.name}</li>
-                  ))}
-                </ul>
-              </Box>
-            )}
-            <DialogActions id="add-to-list-action-button">
-              <Box
-                id="agregar-a-list-button-container"
-                className="agregar-a-list-button-container"
-              >
-                <Button
-                  onClick={onAddList}
-                  id="recipe-dialog-add-to-list-button"
-                >
-                  <Typography variant="h6" color="white">
-                    Agregar a lista
-                  </Typography>
-                </Button>
-              </Box>
-            </DialogActions>
+            <Box>
+              <Typography variant="h6" gutterBottom>
+                Ingredients:
+              </Typography>
+              <ul>
+                {recipe.items?.map((item, index) => (
+                  <li key={index}>{item.name}</li>
+                ))}
+              </ul>
+            </Box>
           </Box>
         </Grid>
+
+        <DialogActions id="add-to-list-action-button">
+          <Box
+            id="agregar-a-list-button-container"
+            className="agregar-a-list-button-container"
+          >
+            <Button onClick={onAddList} id="recipe-dialog-add-to-list-button">
+              <Typography variant="h6" color="white">
+                Agregar a lista
+              </Typography>
+            </Button>
+          </Box>
+        </DialogActions>
       </DialogContent>
     </Dialog>
   );
