3715c6ea8106f3a6feeb93f05cdc2bad1941ec75
Luis <luisjosepr@gmail.com>
2024-11-26 10:08:49 -0300

    hotfix

--- src/app/(ahorratonLayout)/components/list/MoreOptions.tsx
+++ src/app/(ahorratonLayout)/components/list/MoreOptions.tsx
@@ -1,16 +1,15 @@
-import { Box, IconButton } from "@mui/material";
+import { Box, IconButton, Link } from "@mui/material";
 import React from "react";
 import SettingsIcon from "@mui/icons-material/Settings";
 
 export default function MoreOptions() {
   return (
     <Box textAlign="center" m={2}>
-      <IconButton
-        color="primary"
-        onClick={() => (window.location.href = `/miLista`)}
-      >
-        <SettingsIcon />
-      </IconButton>
+      <Link href="/miLista">
+        <IconButton color="primary">
+          <SettingsIcon />
+        </IconButton>
+      </Link>
     </Box>
   );
 }
