437d003b549a028324b9514fdeb8c71c4f36ebbf
Merok23 <alejocalizblanco@gmail.com>
2024-12-07 14:16:42 -0300

    feat: add a modal to the filters so they aren't inmediately in the way hiding the products.

--- src/app/miLista/page.tsx
+++ src/app/miLista/page.tsx
@@ -1,19 +1,35 @@
 "use client";
-
-import { Box, Breadcrumbs, Link, Typography } from "@mui/material";
+import React, { useState } from "react";
+import { Box, Button, Breadcrumbs, Link, Typography } from "@mui/material";
+import TuneIcon from "@mui/icons-material/Tune";
 import MiLista from "./MiLista";
 
 export default function Page() {
+  const [filterModal, setFilterModal] = useState<boolean>(false);
   return (
     <Box>
       <Box mt={5}>
-        <Breadcrumbs aria-label="breadcrumb">
-          <Link color="inherit" href="/">
-          <strong>Inicio</strong>
-          </Link>
-          <Typography color="textPrimary"><strong>Mi lista</strong></Typography>
-        </Breadcrumbs>
-        <MiLista />
+        <Box display='flex' flexDirection='row' justifyContent='space-between' alignItems='center'>
+          <Breadcrumbs aria-label="breadcrumb">
+            <Link color="inherit" href="/">
+            <strong>Inicio</strong>
+            </Link>
+            <Typography color="textPrimary"><strong>Mi lista</strong></Typography>
+          </Breadcrumbs>
+          <Button 
+              variant="contained"
+              onClick={() => setFilterModal(true)} 
+              sx={{
+              '&:hover': {
+                backgroundColor: 'darkblue',
+              },
+            }}
+            >
+              <TuneIcon />
+              Filtros
+            </Button>
+        </Box>
+        <MiLista showFilterModal={filterModal} setFilterModal={setFilterModal}/>
       </Box>
     </Box>
   );
