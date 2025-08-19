0d0decad3375c292586995a2b0a13adb039685a6
Luis Paredes <luisjosepr@gmail.com>
2025-08-18 21:29:20 -0300

    use online images first

--- src/app/comparar/page.tsx
+++ src/app/comparar/page.tsx
@@ -132,7 +132,7 @@ const Compare = () => {
     setSuccessStatus(true);
   };
 
-  const [onlyOnlineFilter, setOnlyOnlineFilter] = useState<boolean>(false);
+  const [onlyOnlineFilter, setOnlyOnlineFilter] = useState<boolean>(true);
   const [minPrice, setMinPrice] = useState<number | undefined>(undefined);
   const [maxPrice, setMaxPrice] = useState<number | undefined>(undefined);
   useEffect(() => {
